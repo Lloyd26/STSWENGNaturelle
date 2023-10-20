d004bcb84480a13b99671e95ebd6c1bee70baf45
ashleykylle <118588136+ashleykylle@users.noreply.github.com>
2023-10-21 01:22:01 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,1 +1,22 @@
+const { Schema, SchemaTypes, model } = require('mongoose');
 
+const userSchema = new Schema ({
+    full_name: {
+        type: SchemaTypes.String
+    },
+    contact_num: {
+        type: SchemaTypes.Number
+    },
+    email: {
+        type: SchemaTypes.String
+    },
+    password: {
+        type: SchemaTypes.String,
+        required: true
+    },
+
+})
+
+// for MongoDB collection "users"
+const User = model("users", userSchema, "users");
+module.exports = User;
