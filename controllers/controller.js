da3958d192283651f96975d0378369117cdf150c
Lloyd26 <ademic26@yahoo.com>
2023-10-24 15:36:09 +0800

    Added dynamic '.active' assignment to nav. items

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,10 @@
 const controller = {
     getIndex: function(req, res) {
-        res.render('main', {layout: 'index'});
+        res.render('main', {layout: 'index', active: {home: true}});
     },
 
     getLogin: function(req, res) {
-        res.render('login', {layout: 'index'});
+        res.render('login', {layout: 'index', active: {login: true}});
     }
 }
 
