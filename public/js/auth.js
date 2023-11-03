0cc1f05d6868a16330b5711aa60fd49d6ea8a35f
Lloyd26 <ademic26@yahoo.com>
2023-11-04 01:07:10 +0800

    Optimized form auth functions, Added password length check

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,7 +1,8 @@
 function validateForm(...forms) {
     let valid = true;
     forms.forEach(form => {
-        if (!form.trim().length)
+        form.removeClass("is-invalid");
+        if (!form.val().trim().length)
             valid = false;
         return valid;
     });
@@ -30,50 +31,49 @@ function showError(error_text) {
 
 $(document).ready(function() {
     $("#form-login").on("submit", function(e) {
-        let email = $("#input-email").val();
-        let password = $("#input-password").val();
-
-        let email_input = $("#input-email");
-        let password_input = $("#input-password");
+        let email = $("#input-email");
+        let password = $("#input-password");
 
         let validForm = validateForm(email, password);
 
         if (!validForm) {
             e.preventDefault();
-            email_input.focus();
+            email.focus();
             showError("Please enter your email and password.")
-        } else if (!isEmailValid(email)) {
+        } else if (!isEmailValid(email.val())) {
             e.preventDefault();
-            email_input.focus();
+            email.focus();
             showError("Please enter a valid email address!");
         }
     })
 
     $("#form-register").on("submit", function(e) {
-        let fname_input = $("#input-firstname");
-        let lname_input = $("#input-lastname");
-        let contact_input = $("#input-contactnum");
-        let email_input = $("#input-email");
-        let password_input = $("#input-password");
+        let fname = $("#input-firstname");
+        let lname = $("#input-lastname");
+        let contact_num = $("#input-contactnum");
+        let email = $("#input-email");
+        let password = $("#input-password");
 
-        let email = email_input.val();
-        let password = password_input.val();
-        let contact_num = contact_input.val();
-
-        let validForm = validateForm(email, password);
+        let validForm = validateForm(fname, lname, contact_num, email, password);
 
         if (!validForm) {
             e.preventDefault();
-            email_input.focus();
-            showError("Please enter your email and password.")
-        } else if (!isEmailValid(email)) {
+            showError("Please fill out the form.")
+        } else if (!isEmailValid(email.val())) {
             e.preventDefault();
-            email_input.focus();
+            email.focus();
+            email.addClass("is-invalid");
             showError("Please enter a valid email address!");
-        } else if (!isContactNumValid(contact_num)) {
+        } else if (!isContactNumValid(contact_num.val())) {
             e.preventDefault();
-            contact_input.focus();
-            showError("Please enter a valid contact number!");
+            contact_num.focus();
+            contact_num.addClass("is-invalid");
+            showError("Please follow the contact number format: 09XXXXXXXXX");
+        } else if (password.val().length < 8) {
+            e.preventDefault();
+            password.focus();
+            password.addClass("is-invalid");
+            showError("Password must contain at least 8 characters!");
         }
     })
 })
