8660581294b3e68f6fe36e34740bb137f2ba6085
Lloyd26 <ademic26@yahoo.com>
2023-11-18 02:19:03 +0800

    Added ability to dynamically add services to cart

---
+++ public/js/cart.js
@@ -0,0 +1,60 @@
+import {Element} from "./element.js";
+
+export function setCartDateTime(date, time) {
+    document.getElementById("date-detail").innerText = date;
+    document.getElementById("time-detail").innerText = time;
+}
+
+export function addToCart(service, staff, details) {
+    let cart_item = new Element(".cart-item").getElement();
+
+    let cart_service_details = new Element(".cart-service-details").getElement();
+
+    let cart_service_name = new Element(".cart-service-name", {
+        text: service
+    }).getElement();
+
+    let preferred_staff_container = new Element("div").getElement();
+
+    let preferred_staff = new Element("span", {
+        text: "Preferred Staff:"
+    }).getElement()
+
+    let cart_staff_name = new Element("span.cart-staff-name", {
+        text: staff
+    }).getElement();
+
+    preferred_staff_container.append(preferred_staff, cart_staff_name);
+
+    let cart_other_details = new Element(".cart-other-details", {
+        text: details
+    }).getElement();
+
+    cart_service_details.append(cart_service_name, preferred_staff_container, cart_other_details);
+
+    let cart_price_label = new Element(".cart-price-label").getElement();
+
+    let cart_price_currency = new Element("span.cart-price-currency", {
+        text: "₱"
+    }).getElement();
+
+    let cart_price_value = new Element("span.cart-price-value", {
+        text: "500.00"
+    }).getElement();
+
+    let edit_btn_container = new Element(".edit-btn-container").getElement();
+
+    let edit_btn_add = new Element("button.btn.edit.add-btn", {
+        text: "+"
+    }).getElement();
+
+    let edit_btn_remove = new Element("button.btn.edit.remove-btn", {
+        text: "-"
+    }).getElement();
+
+    edit_btn_container.append(edit_btn_add, edit_btn_remove);
+
+    cart_item.append(cart_service_details, cart_price_label, edit_btn_container);
+
+    document.getElementById("services-container").appendChild(cart_item);
+}
--- public/js/serviceform.js
+++ public/js/serviceform.js
@@ -1,23 +1,31 @@
 import {showError, showSuccess} from "./form.js";
+import {addToCart} from "./cart.js";
 
 $(document).ready(function(){
     $("#form-service").on("submit", function(e) {
-        let service = $('#input-service').val()
-        let staff = $('#input-staff').val()
-        let details = $('#input-details').val()
-        console.log(service,staff)
-        if ($('#input-service').prop('disabled')){
+        let service_val = $('#input-service').val()
+        let staff_val = $('#input-staff').val()
+        let details_val = $('#input-details').val()
+
+        if ($('#input-service').prop('disabled')) {
             e.preventDefault();
             showError("Select a valid reservation date first.", "#add-error-msg");
-        } else if (!service){
+        } else if (!service_val) {
             e.preventDefault();
             showError("Please select an service to put in the cart.", "#add-error-msg");
-        } else if (!staff){
+        } else if (!staff_val) {
             e.preventDefault();
             showError("Please select a valid Preferred Staff option.", "#add-error-msg");
         } else {
             e.preventDefault();
-            showSuccess("Added to cart successfully!", "#add-error-msg")
+            showSuccess("Added to cart successfully!", "#add-error-msg");
+
+            let service_select = document.getElementById("input-service");
+            let staff_select = document.getElementById("input-staff");
+
+            let service = service_select.options[service_select.selectedIndex].text;
+            let staff = staff_select.options[staff_select.selectedIndex].text;
+            addToCart(service, staff, details_val);
         }
     });
 });
