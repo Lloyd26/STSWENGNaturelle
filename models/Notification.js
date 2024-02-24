804806ff82a0ec10dfd58a302fe15054e81fa9f3
Franciz Emmanuelle Angelo Esperanza <franciz_esperanza@dlsu.edu.ph>
2024-02-24 16:41:36 +0800

    create Notification model

---
+++ models/Notification.js
@@ -0,0 +1,24 @@
+const { Schema, SchemaTypes, model, SchemaType } = require('mongoose');
+
+const notificationSchema = new Schema ({
+    receiver: {
+        type: SchemaTypes.ObjectId,
+        ref: "User"
+    },
+    timestamp: {
+        type: SchemaTypes.String
+    },
+    title: {
+        type: SchemaTypes.String
+    },
+    body: {
+        type: SchemaTypes.String
+    },
+    isRead: {
+        type: SchemaTypes.Boolean
+    }
+
+})
+
+const Notification = model("Notification", notificationSchema, "notification");
+module.exports = SpecialService;
