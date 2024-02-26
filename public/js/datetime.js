625acc0beb91d1c50ed2a2f233a1a5070b8e344c
Lloyd26 <ademic26@yahoo.com>
2024-02-26 23:14:27 +0800

    Added "datetime.js" for easy datetime formatting

---
+++ public/js/datetime.js
@@ -0,0 +1,58 @@
+export function formatDateTime(date, format) {
+    if (!date instanceof Date) {
+        throw new Error("Parameter \"date\" is not an instance of the Date object.");
+    }
+
+    const y = date.getFullYear();
+    const M = date.getMonth();
+    const d = date.getDate();
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    const monthNamesFull = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+    const dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const dayNamesFull = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    const dateTimeFormatMap = new Map();
+
+    dateTimeFormatMap.set("yyyy", y);
+    dateTimeFormatMap.set("yy", padZero(y % 100, 10));
+    dateTimeFormatMap.set("y", y % 100);
+
+    dateTimeFormatMap.set("MMMM", monthNamesFull[M]);
+    dateTimeFormatMap.set("MMM", monthNames[M]);
+    dateTimeFormatMap.set("MM", padZero(M + 1, 10));
+    dateTimeFormatMap.set("M", M + 1);
+
+    dateTimeFormatMap.set("dddd", dayNamesFull[date.getDay()]);
+    dateTimeFormatMap.set("ddd", dayNames[date.getDay()]);
+
+    dateTimeFormatMap.set("dd", padZero(d, 10));
+    dateTimeFormatMap.set("d", d);
+
+    dateTimeFormatMap.set("hh", (h + 1) > 12 ? padZero(h - 12 + 1, 10) : padZero(h + 1, 10));
+    dateTimeFormatMap.set("h", (h + 1) > 12 ? (h - 12 + 1) : (h + 1));
+    dateTimeFormatMap.set("HH", padZero(h, 10));
+    dateTimeFormatMap.set("H", h);
+
+    dateTimeFormatMap.set("mm", padZero(m, 10));
+    dateTimeFormatMap.set("m", m);
+
+    dateTimeFormatMap.set("ss", padZero(s, 10));
+    dateTimeFormatMap.set("s", s);
+
+    dateTimeFormatMap.set("tt", h >= 12 ? "PM" : "AM");
+    dateTimeFormatMap.set("t", h >= 12 ? "P" : "A");
+
+    let formatted = format;
+    dateTimeFormatMap.forEach((v, k) => formatted = formatted.replaceAll("%" + k, v));
+
+    return formatted;
+}
+
+function padZero(n, threshold) {
+    return n < threshold ? "0" + n : n;
+}
