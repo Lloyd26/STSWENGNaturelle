f24e4fffc249fcc03461aa88eabd6983163ed7df
charles <charles_hinolan@dlsu.edu.ph>
2024-03-04 20:40:57 +0800

    added more render tests

--- scripts/gentest.test.js
+++ scripts/gentest.test.js
@@ -5,3 +5,29 @@ test('Salon website renders without errors',async () => {
     expect(response.status).toBe(200);
 
   });
+
+
+test('Salon login page renders without errors',async () => {
+
+    const response = await fetch('https://stswengnaturelle.onrender.com/login');
+    const body = await response.text();
+    expect(response.status).toBe(200);
+
+  });
+
+test('Salon register page renders without errors',async () => {
+
+    const response = await fetch('https://stswengnaturelle.onrender.com/register');
+    const body = await response.text();
+    expect(response.status).toBe(200);
+
+  });
+
+test('Salon about page renders without errors',async () => {
+
+    const response = await fetch('https://stswengnaturelle.onrender.com/about');
+    const body = await response.text();
+    expect(response.status).toBe(200);
+
+  });
+
